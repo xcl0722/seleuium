8af7d036a58aa9e4826e8365219a8fcb8d468c72
xcl <xcl0722@qq.com>
2021-01-22 14:40:53 +0800

    add test case

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 git init
 git add -A
-git commit -m "first commit"
+git commit -m "add test case"
 git branch -M main
 git remote add origin https://github.com/xcl0722/seleuium.git
 git push -f -u origin main
